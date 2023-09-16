04f63efedc3fdfd69bde8aa0768cc1617c791bbe
Hegazy <h3g4zy@gmail.com>
2023-09-16 20:11:08 +0300

    28802

--- _printf.c
+++ _printf.c
@@ -1,63 +1,48 @@
 #include "main.h"
 
-/**
- * _printf - Custom printf function
- * @format: Format string
- *
- * Return: Number of characters printed (excluding the null byte)
- */
-
-int _printf(const char *format, ...)
-{
+int _printf(const char *format, ...) {
     int char_count = 0;
 
     va_list args;
     va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
+
+
+    while (*format) {
+        if (*format == '%') {
             format++;
-            switch (*format)
-            {
-                case 'c':
-                {
+            switch (*format) {
+                case 'c': {
                     char c = va_arg(args, int);
                     putchar(c);
                     char_count++;
                     break;
                 }
-                case 's':
-                {
+                case 's': {
                     char *str = va_arg(args, char *);
-                    while (*str)
-                    {
+                    while (*str) {
                         putchar(*str);
                         str++;
                         char_count++;
                     }
                     break;
                 }
-                case '%':
+                case '%': {
                     putchar('%');
                     char_count++;
                     break;
+                }
                 default:
-                    putchar('%');
-                    putchar(*format);
-                    char_count += 2;
                     break;
             }
-        }
-        else
-        {
+        } else {
             putchar(*format);
             char_count++;
         }
-        format++; 
+        format++;
     }
 
     va_end(args);
+
     return char_count;
 }
--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
+
+int _putchar(char c);
 int _printf(const char *format, ...);
 
-#endif /* MAIN_H */
+#endif
