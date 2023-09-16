9b84d092ce0e9f63426d6c076ae91410409c4681
Hegazy <h3g4zy@gmail.com>
2023-09-16 20:22:32 +0300

    3361

--- main.h
+++ main.h
@@ -2,11 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
-#include <string.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
