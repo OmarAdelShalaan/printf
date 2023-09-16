8f433883e036098332380b44e2e814e78a1f7499
Hegazy <h3g4zy@gmail.com>
2023-09-16 20:12:38 +0300

    29622

--- _printf.c
+++ _printf.c
@@ -1,34 +1,38 @@
 #include "main.h"
 
+/**
+ * _printf - Prints formatted output to stdout
+ * @format: The format string containing directives
+ *
+ * Return: The number of characters printed (excluding the null byte)
+ */
+
 int _printf(const char *format, ...) {
     int char_count = 0;
 
     va_list args;
     va_start(args, format);
-
-
-
     while (*format) {
         if (*format == '%') {
             format++;
             switch (*format) {
                 case 'c': {
                     char c = va_arg(args, int);
-                    putchar(c);
+                    _putchar(c);
                     char_count++;
                     break;
                 }
                 case 's': {
                     char *str = va_arg(args, char *);
                     while (*str) {
-                        putchar(*str);
+                        _putchar(*str);
                         str++;
                         char_count++;
                     }
                     break;
                 }
                 case '%': {
-                    putchar('%');
+                    _putchar('%');
                     char_count++;
                     break;
                 }
@@ -36,7 +40,7 @@ int _printf(const char *format, ...) {
                     break;
             }
         } else {
-            putchar(*format);
+            _putchar(*format);
             char_count++;
         }
         format++;
